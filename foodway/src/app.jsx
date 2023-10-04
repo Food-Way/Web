4c3ac97a400d6d36bdeea1e3483b653a8fcdead9
Fernandeess <matheus.rodrigues@sptech.school>
2023-10-04 19:33:46 -0300

    feat:Rotas do React Router Dom

---
+++ foodway/src/app.jsx
@@ -0,0 +1,12 @@
+import Rotas from "./routes"
+
+const App = () => {
+
+return(
+    <>
+    <Rotas/>
+    </>
+)
+
+}
+export default App;
--- foodway/src/main.jsx
+++ foodway/src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Rotas from './routes'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './app'
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <App/>
+  </React.StrictMode>
+);
