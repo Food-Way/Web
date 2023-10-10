0845bd9c70b6de32de446cf474478968e1e4ab5f
Fernandeess <matheus.rodrigues@sptech.school>
2023-10-10 10:46:00 -0300

    utilizando as variaveis nos components styles

--- foodway/src/components/Header/HeaderGeneral.jsx
+++ foodway/src/components/Header/HeaderGeneral.jsx
@@ -1,33 +1,32 @@
-import LogoFoodWay from  '../../assets/img/FoodWayLogo.png';
-import SelectCategory from '../SelectCategory/SelectCategory';
-import SearchBar from '../SearchBar/SearchBar';
-import './HeaderGeneral.css';
-
+import LogoFoodWay from "../../assets/img/FoodWayLogo.png";
+import SelectCategory from "../SelectCategory/SelectCategory";
+import SearchBar from "../SearchBar/SearchBar";
+import "./HeaderGeneral.css";
 
 const HeaderGeneral = () => {
-    return (
-        <>
-            <header>
-                <div className='container'>
-                    <div className='left'>
-                        <img src={LogoFoodWay} alt="Logo FoodWay" />
-                        <h1>FoodWay</h1>
-                    </div>
-                    <SelectCategory />
-                    <SearchBar defaultMSG="Busque Restaurantes" />
-                    {/* Select component local */}
-                    <nav>
-                        <ul>
-                            <li>Inicio</li>
-                            <li>Sobre</li>
-                            <li>Login</li>
-                            <li className='active'>Cadastro</li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
-        </>
-    );
-}
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="left">
+            <img src={LogoFoodWay} alt="Logo FoodWay" />
+            <h1>FoodWay</h1>
+          </div>
+          <SelectCategory />
+          <SearchBar defaultMSG="Busque Restaurantes" />
+          {/* Select component local */}
+          <nav>
+            <ul>
+              <li>Inicio</li>
+              <li>Sobre</li>
+              <li>Login</li>
+              <li className="active">Cadastro</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
 
 export default HeaderGeneral;
