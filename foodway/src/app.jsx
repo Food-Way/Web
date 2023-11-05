4a380342668512b789ce9ba182271b73d8354ae4
leobr1t0 <leonardo.oliveira@sptech.school>
2023-11-05 14:41:02 -0300

    merge

--- foodway/src/app.jsx
+++ foodway/src/app.jsx
@@ -1,24 +1,11 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-import NotFound from "./pages/NotFound";
-import Home from "./pages/Home/Index";
-const App = () => {
+import Header from './components/HeaderGeneral/HeaderGeneral.jsx';
 
-return(
-    <BrowserRouter>
-      <Routes>
-        <Route Component={Home} path="/home" />
-        <Route path="/sign-in" />
-        <Route path="/sign-up-costumer" />
-        <Route path="/sign-up-establishment" />
-        <Route path="/perfil" />
-        <Route path="/establishment" />
-        <Route path="/menu" />
-        <Route path="/establishment/menu/edit" />
-        <Route path="/establishment/performance" />
-        <Route Component={NotFound} path="*" />
-      </Routes>
-    </BrowserRouter>
-)
-
+function App() {
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
 }
+
 export default App;
