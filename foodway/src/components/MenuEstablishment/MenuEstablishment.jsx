714db19de9c29d3e30a9132755d8e899a15aaa2f
Samuel-Oli-P <samuel.pereira@sptech.school>
2023-11-05 13:16:46 -0300

    Fix: Adjust general

---
+++ foodway/src/components/MenuEstablishment/MenuEstablishment.jsx
@@ -0,0 +1,20 @@
+import { useState, React, useEffect } from "react";
+import './MenuEstablishment.css';
+import api from '../../services/auth';
+
+function MenuEstablishment() {
+    return(
+        <>
+        <aside>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <span></span>
+                </div>
+            </div>
+        </aside>
+        </>
+    );
+}
+
+export default MenuEstablishment;
---
+++ foodway/src/pages/SearchEstablishment/SearchEstablishment.jsx
@@ -0,0 +1,14 @@
+import { useState, React, useEffect } from "react";
+import './SearchEstablishment.css';
+import api from '../../services/auth';
+import MenuEstablishment from "../../components/MenuEstablishment/MenuEstablishment";
+
+function SearchEstablishment() {
+    return (
+        <>
+            <MenuEstablishment />
+        </>
+    );
+}
+
+export default SearchEstablishment;
