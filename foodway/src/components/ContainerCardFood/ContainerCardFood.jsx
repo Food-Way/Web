13ed48599cfff42f560909964bce0c044bb3ca6d
Fernandeeess <matheus.rodrigues@sptech.school>
2024-02-11 12:59:27 -0300

    fix: chamada com skeleton loader

--- foodway/src/components/ContainerCardFood/ContainerCardFood.jsx
+++ foodway/src/components/ContainerCardFood/ContainerCardFood.jsx
@@ -7,65 +7,68 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import api from "../../services/api";
+import { SmsOutlined } from "@material-ui/icons";
 
-const ContainerCardFood = (props) => {
+import ContentLoader from "react-content-loader";
+
+const ContainerCardFood = ({ categories }) => {
+
+
 
   const [tamanhoDoElemento, setTamanhoDoElemento] = useState(0);
 
   const calcularSlidesPorVisualizacao = (tamanhoDoElemento) => {
     if (tamanhoDoElemento > 1300) {
-      // Telas muito grandes (1201px e acima)
-      return 6; // Número de slides para telas muito grandes
+      return 6;
     } else if (tamanhoDoElemento <= 1300 && tamanhoDoElemento >= 800) {
       return 3;
     } else if (tamanhoDoElemento <= 800) {
       return 2;
     }
   };
-  const slidesPorVisualizacao =
-    calcularSlidesPorVisualizacao(tamanhoDoElemento);
+
+  const slidesPorVisualizacao = calcularSlidesPorVisualizacao(tamanhoDoElemento);
 
   useEffect(() => {
-    console.log(props)
-    // Função para calcular o tamanho do elemento
     const calcularTamanhoDoElemento = () => {
       const elementoAlvo = document.getElementById("carrosselId");
-
       if (elementoAlvo) {
-        const tamanho = elementoAlvo.getBoundingClientRect().width; // Substitua "width" pela propriedade desejada (e.g., "height")
-
+        const tamanho = elementoAlvo.getBoundingClientRect().width;
         setTamanhoDoElemento(tamanho);
       }
     };
 
-    // Chama a função inicialmente
     calcularTamanhoDoElemento();
-
-    // Adiciona um ouvinte de redimensionamento da janela para atualizar o tamanho quando a janela for redimensionada
     window.addEventListener("resize", calcularTamanhoDoElemento);
 
     return () => {
-      // Remove o ouvinte de redimensionamento ao desmontar o componente
       window.removeEventListener("resize", calcularTamanhoDoElemento);
     };
   }, []);
 
+  
+
   return (
     <>
       <div className="carrossel-typefood">
-        <Swiper
-          className="carrossel-container-typefood"
-          id="carrosselId"
-          loop={true}
-          modules={[Navigation, Pagination, Scrollbar, A11y]}
-          spaceBetween={5}
-          slidesPerView={slidesPorVisualizacao}
-          navigation
-        >
-          <SwiperSlide>
-            <CardTypeFood typeFood={props.typeFood} image={props.image} />
-          </SwiperSlide>
-        </Swiper>
+       
+          <Swiper
+            className="carrossel-container-typefood"
+            id="carrosselId"
+            loop={true}
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={5}
+            slidesPerView={slidesPorVisualizacao}
+            navigation
+          >
+            {categories.map((item) => (
+              <SwiperSlide key={item.id}>
+                <CardTypeFood typeFood={item.name} image={item.photo} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+       
       </div>
     </>
   );
