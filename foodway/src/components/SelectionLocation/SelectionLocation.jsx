665481f4ee6a74730c43c767dc6a2e75fae78e16
leobr1t0 <leonardo.oliveira@sptech.school>
2024-02-20 01:03:44 -0300

    add: select location api call

--- foodway/src/components/SelectionLocation/SelectionLocation.jsx
+++ foodway/src/components/SelectionLocation/SelectionLocation.jsx
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
-import locations from './location.json'
+import React, { useEffect, useState } from 'react';
 import './SelectionLocation.css';
+import api_call from '../../services/apiImpl';
 
 function SelectLocation() {
-    const [location, setLocation] = useState(locations);
+    const [location, setLocation] = useState([]);
+
+    async function getLocations() {
+        const response = await api_call("get", "/states", null, null);
+        console.log(response);
+        setLocation(response);
+    }
 
     function showOptions() {
         var options = document.querySelector('.options-location');
@@ -16,23 +22,31 @@ function SelectLocation() {
         }
     }
 
+    useEffect(() => {
+        getLocations();
+    })
+
     return (
         <>
             <div className='select-location' onClick={showOptions}>
                 <div>
                     <span id='location-text'>Localização</span>
                     <div className='options-location hide'>
-                        {location.map((item) => {
-                            return (
-                                <div key={item.id} onClick={() => {
-                                    showOptions();
-                                    var newLocation = document.getElementById('location-text');
-                                    newLocation.innerHTML = item.name;
-                                }}>
-                                    <span>{item.name}</span>
-                                </div>
-                            )
-                        })}
+                        {location.length === 0 ? (
+                            <span>Nenhum resultado</span>
+                        ) : (
+                            location.map((item) => {
+                                return (
+                                    <div key={item.id} onClick={() => {
+                                        showOptions();
+                                        var newLocation = document.getElementById('location-text');
+                                        newLocation.innerHTML = item.name;
+                                    }}>
+                                        <span>{item.name}</span>
+                                    </div>
+                                )
+                            })
+                        )}
                     </div>
                 </div>
             </div>
