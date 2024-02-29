ca2ee2e06eb702cfa6b37679f98c6ac5a3da7978
Samuel-Oli-P <samuel.pereira@sptech.school>
2024-02-28 21:45:14 -0300

    Feat: add report download

--- foodway/src/components/Report/Report.jsx
+++ foodway/src/components/Report/Report.jsx
@@ -1,23 +1,69 @@
-import React from "react";
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from "react";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import arquivo from "../Report/leo.txt";
+import api_call from "../../services/apiImpl";
+import { useParams } from "react-router-dom";
+import * as XLSX from "xlsx";
 
 import "./Report.css";
 
 const Report = () => {
-    return (
-        <>
-            <div className="report-container">
-                <div className="report-box">
-                    <a href={arquivo} target="_blank" download="leo-coisa.txt">
-                        Relatório - Janeiro
-                        <FontAwesomeIcon icon={faDownload} size="xl" />
-                    </a>
-                </div>
-            </div>
-        </>
-    );
-}
+  const routeParams = useParams();
+  const id = routeParams.id;
+  const [menu, setMenu] = useState([]);
+
+  async function getMenu({ filter }) {
+    const response = await api_call("get", `products/establishments/${id}/${filter}`, null, atob(sessionStorage.getItem("token")));
+    console.log(response);
+    setMenu(response);
+  }
+
+  useEffect(() => {
+    getMenu({ filter: "name" });
+  }, []);
+
+  const downloadExcel = () => {
+    const data = menu.map(item => [item.name, item.idProduct]);
+
+    const ws = XLSX.utils.aoa_to_sheet([["Name", "IdProduct"], ...data]);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+
+    const wbout = XLSX.write(wb, { type: "binary", bookType: "xlsx" });
+
+    const s2ab = (s) => {
+      const buf = new ArrayBuffer(s.length);
+      const view = new Uint8Array(buf);
+      for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
+      return buf;
+    };
+
+    const fileName = "relatorio.xlsx";
+
+    if (typeof window !== "undefined") {
+      const blob = new Blob([s2ab(wbout)], { type: "application/octet-stream" });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = fileName;
+      a.click();
+      window.URL.revokeObjectURL(url);
+    }
+  };
+
+  return (
+    <>
+      <div className="report-container">
+        <div className="report-box">
+          <button onClick={downloadExcel}>
+            Relatório - Janeiro
+            <FontAwesomeIcon icon={faDownload} size="xl" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Report;
