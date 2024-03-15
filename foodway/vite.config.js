cd6d3e1f638d8482f09232818a06fd4d3543fae8
leobr1t0 <leonardo.oliveira@sptech.school>
2024-03-15 20:54:52 -0300

    adjust prod url to private ec2

--- foodway/vite.config.js
+++ foodway/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Permite conexões externas
+    host: '0.0.0.0',
+    // Especifica a porta, opcionalmente, se você quiser forçar uma porta diferente de 5173
+    port: 5173,
+    // Configurações adicionais do servidor podem ser adicionadas aqui
+  },
 })
