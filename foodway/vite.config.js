89954735bac80a28e1dc78d43073e1a7b0bbd200
Fernandeeess <matheus.rodrigues@sptech.school>
2024-05-05 18:28:53 -0300

    removing images & adding docker configuration

--- foodway/vite.config.js
+++ foodway/vite.config.js
@@ -2,21 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://10.0.0.243:8080/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/nifi': {
-        target: 'http://localhost:80/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/nifi/, ''),
-        secure: false,
-      },
-    },
-  },
+  
   plugins: [react()],
   cors: true,
 })
