77591ba0b9723a4270b12dbd307a628ac1449642
leobr1t0 <leonardo.oliveira@sptech.school>
2024-05-27 00:40:36 -0300

    test ci/cd with deploy

--- foodway/src/components/MainBanner/MainBanner.jsx
+++ foodway/src/components/MainBanner/MainBanner.jsx
@@ -7,6 +7,15 @@ const MainBanner = () => {
 
   return (
     <>
+      <div style={styleCard} className="main-banner-container">
+        <h1>
+          <span>
+            FoodWay: o caminho para{" "}
+            <span id="red">Experiências Gastronômicas</span>
+          </span>
+          <span>autênticas, apoiado por avaliações reais</span>
+        </h1>
+      </div>
     </>
   );
 };
